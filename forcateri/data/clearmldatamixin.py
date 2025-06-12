54924db16b68a729f1e7d145747bd7661eb460a3
dior01 <dior01@dfki.de>
2025-06-12 15:58:42 +0200

    updates as per #28

--- forcateri/data/clearmldatamixin.py
+++ forcateri/data/clearmldatamixin.py
@@ -1,12 +1,22 @@
+from clearml import Dataset, Task
+
 
 class ClearmlDataMixin:
-    def __init__(self, dataset_project:str, dataset_name:str, dataset_version:str, **kwargs):
+    def __init__(
+        self, dataset_project: str, dataset_name: str, dataset_version: str, **kwargs
+    ):
         super().__init__(**kwargs)
         self.dataset_project = dataset_project
         self.dataset_name = dataset_name
         self.dataset_version = dataset_version
-        
-    def get_from_clearml():
-        pass 
+
+    def get_from_clearml(self):
+        data_root = Dataset.get(
+            dataset_project=self.dataset_project,
+            dataset_name=self.dataset_name,
+            dataset_version=self.dataset_version,
+        )
+        return data_root
+
     def update_on_clearml():
         pass
