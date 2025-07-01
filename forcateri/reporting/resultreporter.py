c7dc2d98276e3f296dfe6943730d81c291688800
Dinar <dior01@dfki.de>
2025-07-01 11:48:59 +0200

    initial skeleton of reporting class #31

---
+++ forcateri/reporting/resultreporter.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+from .metric import Metric
+from ..data.adapterinput import AdapterInput
+from ..model.modeladapter import ModelAdapter
+
+
+class ResultReporter:
+
+    def __init__(
+        test_data: List[AdapterInput], models: List[ModelAdapter], metrics: List[Metric]
+    ):
+        pass
+
+    def report_all(self):
+        pass
+
+    def __compute_metrics():
+        pass
+
+    def __select_debug_samples():
+        pass
+
+    def __report_metrics():
+        pass
+
+    def __create_plots():
+        pass
+
+    def __report_plots():
+        pass
+
+    def __report_debug_samples():
+        pass
+
+    def __persist_artifacts():
+        pass
