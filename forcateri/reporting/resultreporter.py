b9f84b8791f2795343b7b8e62ca4b0b8e2fa205b
dior01 <dior01@dfki.de>
2025-07-03 16:15:04 +0200

    initial logic for plots #31

--- forcateri/reporting/resultreporter.py
+++ forcateri/reporting/resultreporter.py
@@ -1,5 +1,6 @@
 from typing import List
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from .metric import Metric
 from ..data.adapterinput import AdapterInput
@@ -8,10 +9,12 @@ from ..model.modeladapter import ModelAdapter
 
 class ResultReporter:
 
-    def __init__(
+    def __init__(self,
         test_data: List[AdapterInput], models: List[ModelAdapter], metrics: List[Metric]
     ):
-        pass
+        self.test_data = test_data
+        self.models = models
+        self.metrics = metrics
 
     def report_all(self):
         pass
@@ -25,8 +28,18 @@ class ResultReporter:
     def __report_metrics():
         pass
 
-    def __create_plots():
-        pass
+    def __create_plots(self,df):
+
+        for offset_value, group_df in df.groupby(level='offset'):
+            ax = group_df.droplevel('offset').plot(
+                marker='o', 
+                title=f'Offset: {offset_value}', 
+                xlabel='time_stamp', 
+                ylabel='Value'
+            )
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()
 
     def __report_plots():
         pass
