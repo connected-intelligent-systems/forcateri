44e1b2f2b0440f882f44b4244d52e4949a0c7bb8
dior01 <dior01@dfki.de>
2025-07-04 15:14:57 +0200

    update on create_plot() #31

--- forcateri/reporting/resultreporter.py
+++ forcateri/reporting/resultreporter.py
@@ -16,8 +16,8 @@ class ResultReporter:
         self.models = models
         self.metrics = metrics
 
-    def report_all(self):
-        pass
+    def report_all(self,df):
+        self.__create_plots(df)
 
     def __compute_metrics():
         pass
@@ -28,19 +28,32 @@ class ResultReporter:
     def __report_metrics():
         pass
 
-    def __create_plots(self,df):
+    def __create_plots(self):
 
-        for offset_value, group_df in df.groupby(level='offset'):
-            ax = group_df.droplevel('offset').plot(
-                marker='o', 
-                title=f'Offset: {offset_value}', 
-                xlabel='time_stamp', 
-                ylabel='Value'
-            )
-            plt.xticks(rotation=45)
-            plt.tight_layout()
-            plt.show()
+        for model in self.models:
+            predictions = model.predict(self.test_data)
+            predictions_ts_list = model.to_time_series(predictions)  # List of TimeSeries objects
 
+            for i, (adapter_input, pred_ts) in enumerate(zip(self.test_data, predictions_ts_list)):
+                gt_ts = adapter_input.target  # Also a TimeSeries object
+
+                
+                offsets = pred_ts.data.index.get_level_values("offset").unique()
+
+                for offset in offsets:
+                    pred_df = pred_ts.by_time(offset)
+                    gt_df = gt_ts.by_time(offset)
+
+                    # Align on timestamps
+                    pred_df_aligned, gt_df_aligned = pred_df.align(gt_df, join='inner')
+
+                    if pred_df_aligned.empty:
+                        continue  # No overlapping timestamps
+
+                    ax = pred_df_aligned.plot(label='Prediction', title=f"Sample {i}, Model: {model.name}, Offset: {offset}")
+                    gt_df_aligned.plot(ax=ax, label='Ground Truth', linestyle='--', alpha=0.7)
+                    ax.legend()
+        
     def __report_plots():
         pass
 
