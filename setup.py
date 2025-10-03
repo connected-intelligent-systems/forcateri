8a7675fb3ba2e1f18165548571ad8bf9d7889b33
Dinar <dior01@dfki.de>
2025-10-03 20:28:36 +0200

    test

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="forcateri",       # package name for import
-    version="0.2",
+    version="0.3",
     packages=find_packages(),  
 )
