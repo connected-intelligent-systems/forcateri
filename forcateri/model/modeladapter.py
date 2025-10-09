1478457dbcb15050f4d0998b3184ca69dfef02ba
Alexander Anisimov <alexander.anisimov@dfki.de>
2025-10-09 13:58:13 +0200

    Dropped @staticmethod annotations to avoid forcing overrides.

--- forcateri/model/modeladapter.py
+++ forcateri/model/modeladapter.py
@@ -1,20 +1,13 @@
 import datetime
-from abc import ABC, abstractmethod
+from abc import ABC
 from pathlib import Path
 from typing import Any, List, Optional, Union
-
-from forcateri.data import adapterinput
 
 from ..data.adapterinput import AdapterInput
 from ..data.timeseries import TimeSeries
 
 
 class ModelAdapter(ABC):
-    @abstractmethod
-    def __init__(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
     def fit(
         self,
         train_data: List[AdapterInput],
@@ -25,13 +18,11 @@ class ModelAdapter(ABC):
             "Method not overridden in concrete adapter implementation"
         )
 
-    @abstractmethod
     def predict(self, data: List[AdapterInput]):
         raise NotImplementedError(
             "Method not overridden in concrete adapter implementation"
         )
 
-    @abstractmethod
     def tune(
         self,
         train_data: List[AdapterInput],
@@ -42,19 +33,16 @@ class ModelAdapter(ABC):
             "Method not overridden in concrete adapter implementation"
         )
 
-    @abstractmethod
     def load(self, path: Union[Path, str]) -> None:
         raise NotImplementedError(
             "Method not overridden in concrete adapter implementation"
         )
 
-    @abstractmethod
     def save(self, path: Union[Path, str]) -> None:
         raise NotImplementedError(
             "Method not overridden in concrete adapter implementation"
         )
 
-    @abstractmethod
     def to_model_format(self, ts: TimeSeries) -> Any:
         """
         Applies model-specific transformations to the time series data.
@@ -77,8 +65,7 @@ class ModelAdapter(ABC):
             for i in input
         ]
 
-    @abstractmethod
-    def to_time_series(ts: Any) -> TimeSeries:
+    def to_time_series(self, ts: Any) -> TimeSeries:
         """
         Converts the model-specific data into the standardized TimeSeries format e.g., inverse scaling.
         """
