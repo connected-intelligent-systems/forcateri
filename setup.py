748b63f7fc2c260dcc46daac8a1a1420d37abe0c
dior01 <dior01@dfki.de>
2025-10-19 20:57:20 +0200

    .

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="forcateri",       # package name for import
-    version="0.4.1",
+    version="0.4.2",
     packages=find_packages(),  
 )
