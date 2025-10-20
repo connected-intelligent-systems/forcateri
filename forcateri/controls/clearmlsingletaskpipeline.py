9f39ce386314897c0b7939a2d49687c5095e69a3
dior01 <dior01@dfki.de>
2025-10-20 14:15:53 +0200

    updates as part of #47

--- forcateri/controls/clearmlsingletaskpipeline.py
+++ forcateri/controls/clearmlsingletaskpipeline.py
@@ -10,53 +10,48 @@ from ..data.dataprovider import DataProvider
 from ..reporting.resultreporter import ResultReporter
 
 
-class ClearMlSingleTaskPipeline(Pipeline):
+class ClearMLSingleTaskPipeline(Pipeline):
+
     def __init__(
         self,
         dp: DataProvider,
         model_adapter: Union[ModelAdapter, List[ModelAdapter]],
         reporter: Union[ResultReporter, List[ResultReporter]],
-        config_name: Optional[str] = None,
-        project_root: Optional[Path] = None,
+        config_path: Optional[str] = None,
         project_name: Optional[str] = None,
         task_name: Optional[str] = None,
-        param_kwargs: dict = {},
-        param_args: List = [],
+        param_args: Optional[List] = None,
         requirements: str = None,
+        docker: Optional[str] = None,
     ):
         # self.task_name = task_name
+
         super().__init__(dp, model_adapter, reporter)
-        load_dotenv()
-        self.param_kwargs = param_kwargs
-        self.param_args = param_args
+
+        
+        self.param_args = param_args or []
         self.requirements = requirements
-        if config_name and project_root:
-            self.config_name = config_name
-            self.config = load_config(config_name, project_root)
+        self.config = {}
+        if config_path and os.path.exists(config_path):
+            self.config_path = config_path
+            self.config = load_config(config_path)
+            #upd config based on param_args and project_name and task_name. Param args have priority
+            
             self.args = extract_config(self.config)
             #self.args.append(("config", config_name))
+            if self.param_args:
+                for k, v in self.param_args:
+                    #remove existing arg with same key
+                    self.args = [arg for arg in self.args if arg[0] != k]
+                    self.args.append((k, v))
+        else:
+            self.args = self.param_args
+            
+        
+        self.project_name = project_name if project_name else self.config["ClearML"]["task"]["project_name"]
+        self.task_name = task_name if task_name else self.config["ClearML"]["task"]["task_name"]
+        self.docker = docker if docker else self.config["ClearML"]["task"]["docker"]
 
-            self.project_name = self.config["ClearML"]["task"]["project_name"]
-            self.task_name = self.config["ClearML"]["task"]["task_name"]
-
-            self.branch = self.config["ClearML"]["task"]["branch"]
-            self.repo = self.config["ClearML"]["task"]["repo"]
-            self.script = self.config["ClearML"]["task"]["script"]
-            #     # docker = "nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04",
-            self.docker = self.config["ClearML"]["task"]["docker"]
-
-
-    def execute_remotely(self):
-        token = os.environ["GIT_TOKEN"]
-        self.task.execute_remotely(
-            queue_name="default",
-            clone=False,
-            exit_process=True,
-            docker=self.config["ClearML"]["task"]["docker"],
-            docker_args=(
-                f"-e CLEARML_AGENT_GIT_USER=oauth2 -e CLEARML_AGENT_GIT_PASS={token}"
-            ),
-        )
 
 
         
@@ -66,24 +61,7 @@ class ClearMlSingleTaskPipeline(Pipeline):
         function_task = self.task.create_function_task(func=super().run)
         #function_task.set_base_docker(docker_image=self.docker)
         function_task.set_packages(self.requirements)
-        function_task.connect_configuration(self.param_args)
+        function_task.connect_configuration(self.args)
         Task.enqueue(task=function_task, queue_name="default")
 
-    def execute_task_enq(self,project_name,task_name,script,branch="main",repo="",docker=""):
-        token = os.environ["GIT_TOKEN"]
 
-        self.task = Task.create(
-            project_name=project_name,
-            task_name=task_name,
-            add_task_init_call=True,
-            branch=branch,
-            repo=repo,
-            script=script,
-            # docker = "nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04",
-            docker=docker,
-            docker_args=(
-                f"-e CLEARML_AGENT_GIT_USER=oauth2 -e CLEARML_AGENT_GIT_PASS={token} -e CLEARML_AGENT_SKIP_PYTHON_ENV_INSTALL={self.config['ClearML']['task']['skip_env_install']}"
-            ),
-            argparse_args=self.args,
-        )
-        Task.enqueue(task=self.task, queue_name="default")
