6cc43597affbd489e15e0511d22585865ca9edcb
dior01-dfki <dior01@dfki.de>
2025-11-03 20:57:04 +0100

    changed pkl to csv in clearmlreporter #53

--- forcateri/reporting/clearmlreporter.py
+++ forcateri/reporting/clearmlreporter.py
@@ -25,3 +25,13 @@ class ClearMLReporter(ResultReporter):
         print(f"Test of the metric results {self.metric_results}")
         Task.current_task().upload_artifact(name='Report', artifact_object=self.metric_results)
     
+    def report_metrics(self):
+        super().report_metrics()
+        for model_name, model_results in self.metric_results.items():
+            for metric_name, result_df_list in model_results.items():
+                result = pd.concat(result_df_list, axis=0)
+                result['model'] = model_name
+                result.reset_index(inplace=True)
+                csv_name = f'{model_name}_{metric_name}_results.csv'
+                result.to_csv(csv_name, index=False)
+                Task.current_task().upload_artifact(name=csv_name, artifact_object=csv_name)
