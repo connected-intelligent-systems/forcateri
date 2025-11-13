0b1fec1e61e6a39eeb305af9d92cb5f67d3b7881
dior01-dfki <dior01@dfki.de>
2025-11-13 23:26:28 +0100

    test

--- forcateri/reporting/clearmlreporter.py
+++ forcateri/reporting/clearmlreporter.py
@@ -2,6 +2,7 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 from clearml import Task
+from functools import wraps
 
 from forcateri.reporting.resultreporter import ResultReporter
 from .metric import Metric
@@ -9,6 +10,34 @@ from ..data.adapterinput import AdapterInput
 from ..model.modeladapter import ModelAdapter
 from typing import List
 
+def clearml_log_plot(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        # Run the original plotting function
+        result = func(self, *args, **kwargs)
+
+        # Get ClearML logger
+        task = Task.current_task()
+        if task:
+            logger = task.get_logger()
+
+            # Collect all open figures
+            figs = [plt.figure(n) for n in plt.get_fignums()]
+
+            for i, fig in enumerate(figs):
+                logger.report_matplotlib(
+                    title=f"{func.__name__}_{i}",
+                    series=func.__name__,
+                    figure=fig,
+                    iteration=0,
+                )
+
+            # Close figures after logging
+            for fig in figs:
+                plt.close(fig)
+
+        return result
+    return wrapper
 
 class ClearMLReporter(ResultReporter):
 
@@ -39,3 +68,11 @@ class ClearMLReporter(ResultReporter):
 
             final_df.to_csv(f'{metric_name}_results.csv', index=False)
             Task.current_task().upload_artifact(name=f'{metric_name}_results.csv', artifact_object=final_df)
+    
+    @clearml_log_plot
+    def _plot_metrics(self, metric_results=None):
+        super()._plot_metrics(metric_results)
+    
+    @clearml_log_plot
+    def _plot_predictions(self):
+        super()._plot_predictions()
