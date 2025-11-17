97b1a47c226eb1813e166f75fd858e7754cc6d39
dior01-dfki <dior01@dfki.de>
2025-11-17 12:15:12 +0100

    added logger error logic to quantile metric as part of #46

--- forcateri/reporting/dimwiseaggregatedquantileloss.py
+++ forcateri/reporting/dimwiseaggregatedquantileloss.py
@@ -1,10 +1,10 @@
 from typing import List
-
+import logging
 from .dimwiseaggregatedmetric import DimwiseAggregatedMetric
 from .metric_aggregations import quantile_metric
 from ..data.timeseries import TimeSeries
 
-
+logger = logging.getLogger(__name__)
 class DimwiseAggregatedQuantileLoss(DimwiseAggregatedMetric):
     def __init__(
         self,
@@ -14,6 +14,9 @@ class DimwiseAggregatedQuantileLoss(DimwiseAggregatedMetric):
 
     def __call__(self, ts_gt: TimeSeries, ts_pred: TimeSeries):
         if ts_pred.quantiles is None:
+            logger.error(
+                "Predicted TimeSeries must have quantiles defined for DimwiseAggregatedQuantileLoss."
+            )
             raise ValueError(
                 "Predicted TimeSeries must have quantiles defined for DimwiseAggregatedQuantileLoss."
             )
