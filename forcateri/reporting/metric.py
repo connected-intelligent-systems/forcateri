4032aff6fc58aa518ccd258612cc88f86f7a54cb
Alexander Anisimov <alexander.anisimov@dfki.de>
2026-02-04 19:46:14 +0100

    Renamed parameters and added docstrings To make the usage of `Metric.align` more intuitive.

--- forcateri/reporting/metric.py
+++ forcateri/reporting/metric.py
@@ -1,15 +1,32 @@
-from ..data.timeseries import TimeSeries
-import pandas as pd
 import logging
+from typing import Any, Tuple
+
+import pandas as pd
+
+from ..data.timeseries import TimeSeries
 
 logger = logging.getLogger(__name__)
 
 
 class Metric:
 
-    def align(self, ts_gt: TimeSeries, ts_pred: TimeSeries):
+    def align(self, gt: TimeSeries, pred: TimeSeries) -> Tuple[TimeSeries, TimeSeries]:
+        """Brings ground truth and prediction into a similar format.
 
-        horizon = ts_pred.offsets.max() // pd.Timedelta(1, ts_pred.freq)
+        More precisely:
+            1. Expands ground truth by creating a copy of its data for every offset of the prediction
+            2. Shifts the time index and the offset to align the underlying data with the prediction
+            3. Cuts away parts of the prediction for which there is no ground truth
+            4. Cuts away parts of the ground truth for which there is no prediction
+
+
+        Raises
+        ------
+        ValueError
+            If the prediction offset is < 1
+        """
+
+        horizon = pred.offsets.max() // pd.Timedelta(1, pred.freq)
         logger.debug(
             f"While computing metric the following horizon was calculated on pred_ts: {horizon}"
         )
@@ -20,17 +37,17 @@ class Metric:
                 f"Horizon is expected to be 1 or greater but was {horizon}."
             )
 
-        ts_gt_shifted = ts_gt.shift_to_repeat_to_multihorizon(horizon=horizon)
+        ts_gt_shifted = gt.shift_to_repeat_to_multihorizon(horizon=horizon)
         logger.debug(f"\ngt_shifted:\n{ts_gt_shifted}")
-        common_index = ts_gt_shifted.data.index.intersection(ts_pred.data.index)
+        common_index = ts_gt_shifted.data.index.intersection(pred.data.index)
         logger.debug(
             f"Common index determined to be\n{common_index.to_frame(index=False)}"
         )
         ts_gt_shifted.data = ts_gt_shifted.data.loc[common_index]
-        old_pred_len = len(ts_pred)
-        ts_pred.data = ts_pred.data.loc[common_index]
-        dropped_gt_steps = len(ts_gt) - len(ts_gt_shifted)
-        dropped_pred_steps = old_pred_len - len(ts_pred)
+        old_pred_len = len(pred)
+        pred.data = pred.data.loc[common_index]
+        dropped_gt_steps = len(gt) - len(ts_gt_shifted)
+        dropped_pred_steps = old_pred_len - len(pred)
         if (dropped_gt_steps, dropped_pred_steps) != (0, 0):
             logger.warning(
                 f"Alignment dropped {dropped_gt_steps} time steps ftom the ground truth "
@@ -38,7 +55,14 @@ class Metric:
             )
         else:
             logger.debug("No time steps were dropped during alignment.")
-        return ts_gt_shifted, ts_pred
+        return ts_gt_shifted, pred
 
-    def __call__(self, gt: TimeSeries, pred: TimeSeries):
+    def __call__(self, gt: TimeSeries, pred: TimeSeries) -> Any:
+        """The intended way of trigering a metric computation.
+
+        Makes the class callable.
+        The actual computation logic is taken from the child classes.
+        Consider adding `self.align` to the implementation
+        to make metric instances more generalizable.
+        """
         raise NotImplementedError("Must be overridden in child classes.")
