2a1fa827982d48c38ae961202c5141b18218e7b3
dior01-dfki <dior01@dfki.de>
2026-02-10 11:26:00 +0100

    Made parameter and attribute naming more explicit (#97)
    * renamed dp to data_provider inside init function (ticket #80 PR #97)
    * removed data as part of input for _historical_forecasts method #80
    * adjusted namings of object params in pipeline. Brought back data as part of input for historical_forecasts
    * aligned dartsmodeladapter and modeladapter with the params for public functions. Removed unnecessary reliance on kwargs
    * made the method prepare_predict_args more explicit returning the predict_args as dictionary with target, and other covariates
    * removed parameter n in predict function from ModelAdapter
    * added default none to val_data parameter in fit function
    * updates to make fit and predicts more explicit
    * added warnings to the function get_covariate_args
    * removed static param from get_covariate_args method
    * Removed model adapter kwrgs from class diagram
    * closes #80
    
    ---------
    
    Co-authored-by: Alexander Anisimov <alexander.anisimov@dfki.de>

--- forcateri/reporting/metric_aggregations.py
+++ forcateri/reporting/metric_aggregations.py
@@ -1,21 +1,21 @@
 import numpy as np
 
 
-def column_wise_mae(gt: np.ndarray, pred: np.ndarray) -> np.ndarray:
-    err = gt - pred
+def column_wise_mae(ground_truth: np.ndarray, prediction: np.ndarray) -> np.ndarray:
+    err = ground_truth - prediction
     sq_err = np.abs(err)
     mse = sq_err.mean(axis=0)
     return mse
 
 
 def quantile_metric(
-    gt: np.ndarray,
-    pred: np.ndarray,
+    ground_truth: np.ndarray,
+    prediction: np.ndarray,
     quantiles: np.ndarray,
 ) -> np.ndarray:
-    gt = np.asarray(gt)
-    pred = np.asarray(pred)
+    ground_truth = np.asarray(ground_truth)
+    prediction = np.asarray(prediction)
     quantiles = np.asarray(quantiles)
-    diff = gt - pred
+    diff = ground_truth - prediction
     loss = np.maximum(quantiles * diff, (quantiles - 1) * diff)
     return np.mean(loss, axis=0)
