03a218b342539f7e30591938b227df94ca18b4d5
dior01-dfki <dior01@dfki.de>
2026-02-18 12:50:41 +0100

    removed the logic of saving completely, trying to upload the model directly

--- forcateri/reporting/clearmlreporter.py
+++ forcateri/reporting/clearmlreporter.py
@@ -82,12 +82,9 @@ class ClearMLReporter(ResultReporter):
             name="Model Predictions", artifact_object=self.model_predictions
         )
         for model in self.models:
-            save_path = Path("models") / f"{model.model_name}"
-            model.save(save_path)
-            logger.info(f"Saved model {model.model_name} to {save_path}")
             Task.current_task().upload_artifact(
                 name=model.model_name,
-                artifact_object=f"{save_path}.pkl"
+                artifact_object=model
         )
 
     def report_metrics(self):
