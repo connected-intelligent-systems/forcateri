89d6a53d5e976e05719a3db8a344aabc1689051e
dior01-dfki <dior01@dfki.de>
2026-03-30 14:30:26 +0200

    removed timestamps in the names of each file

--- forcateri/reporting/localresultreporter.py
+++ forcateri/reporting/localresultreporter.py
@@ -35,7 +35,7 @@ class LocalResultReporter(ResultReporter):
         model_dir = self.save_path / self.MODELS_DIR
         model_dir.mkdir(parents=True, exist_ok=True)
         for model in self.models:
-            save_path = model_dir / f"{model.name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+            save_path = model_dir / f"{model.name}"
             model.save(save_path)
             logger.info(f"Saved model {model.name} to {save_path}")
 
@@ -50,9 +50,9 @@ class LocalResultReporter(ResultReporter):
         # save each DataFrame separately if there are multiple
         for i, df in enumerate(self.computed_metrics):
             filename = (
-                metrics_dir / f"all_metrics_results_{i}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+                metrics_dir / f"all_metrics_results_{i}.csv"
                 if len(self.computed_metrics) > 1
-                else metrics_dir / f"all_metrics_results_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+                else metrics_dir / f"all_metrics_results.csv"
             )
             df.to_csv(filename, index=False)
 
@@ -61,7 +61,7 @@ class LocalResultReporter(ResultReporter):
         plots_dir.mkdir(parents=True, exist_ok=True)
         super().plot_metrics()
         for fig, model_name, metric_name in self.metric_plots:
-            fig.write_html(f"{plots_dir}/{model_name}_{metric_name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.html")
+            fig.write_html(f"{plots_dir}/{model_name}_{metric_name}.html")
 
     def plot_predictions(self):
 
@@ -70,7 +70,7 @@ class LocalResultReporter(ResultReporter):
         super().plot_predictions()
         for fig, model_name, test_idx, offset in self.prediction_plots:
             fig.write_html(
-                f"{predictions_plot_dir}/{model_name}_test{test_idx}_offset{offset}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.html"
+                f"{predictions_plot_dir}/{model_name}_test{test_idx}_offset{offset}.html"
             )
     
     def report_predictions(self):
@@ -78,7 +78,7 @@ class LocalResultReporter(ResultReporter):
         for model_name, predictions in self.computed_predictions.items():
             pred_dir = self.save_path / self.PREDICTIONS_DIR
             pred_dir.mkdir(parents=True, exist_ok=True)
-            filename = pred_dir / f"{model_name}_predictions_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.pkl"
+            filename = pred_dir / f"{model_name}_predictions.pkl"
             with open(filename, "wb") as f:
                 pd.to_pickle(predictions, f)
             
